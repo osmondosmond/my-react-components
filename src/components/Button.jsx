1b8c99985200462e7d474cdc826776df6a9fe21d
osmondosmond <osmond@tiket.com>
2022-06-28 16:14:23 +0700

    review built-in storybook files, include tailwind to storybook

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,30 @@
 
 const Button = ({ children, ...others }) => {
-  return <button className="text-sky-600" { ...others } >{children}</button>
+  return (
+    <button
+      className="w-full 
+        flex
+        items-center
+        justify-center
+        px-8
+        py-3
+        border
+        border-transparent
+        text-base
+        font-medium
+        rounded-md
+        text-white
+        bg-indigo-600
+        hover:bg-indigo-700
+        md:bg-sky-600
+        md:py-4
+        md:text-lg
+        md:px-10"
+      {...others}
+    >
+      {children}
+    </button>
+  )
 }
 
 export default Button
